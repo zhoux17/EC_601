1d9ab0915f299d64b94f951a1aaa9772669da4c4
zhoux17 <46795678+zhoux17@users.noreply.github.com>
2020-12-06 22:57:20 -0500

    Update test_twitter.py

--- Project4/test_twitter.py
+++ Project4/test_twitter.py
@@ -1,52 +1,41 @@
-# from TwitterAPI import TwitterAPI, TwitterOAuth
-# import unittest
-# import twitter
+# # import mathfun as fun
+# from pytest import approx
+
+# def test_addints():
+# 	assert 1+1 == 2
+
+# # def test_addfloats():
+# # 	assert fun.add(1.5, 1.7) == approx(3.1)
+
+import unittest
+import twitter
+import os
 
 
-# class OAuthTest(unittest.TestCase):
+class MyTestCase(unittest.TestCase):
+    def test_fetch_successful(self):
+        """
+        test
+        :return:
+        """
+        at = os.getenv('ACCESS_TOKEN')
+        ats = os.getenv('ACCESS_TOKEN_SECRET')
+        ck = os.getenv('CONSUMER_KEY')
+        cs = os.getenv('CONSUMER_SECRET')
 
-#     """Test user and application authentication."""
+        res = twitter_example.fecth_random_tweets(ck, cs, at, ats)
 
-#     def setUp(self):
-#         """Read credentials from TwitterAPI/credentials.txt. You
-#         must copy your credentials into this text file.
-#         """
-#         self.oa = TwitterOAuth.read_file()
-
-#     def test_oauth_1(self):
-#         """Test user authentication."""
-#         api = TwitterAPI(self.oa.CONSUMER_KEY, self.oa.CONSUMER_SECRET,
-#                          self.oa.ACCESS_TOKEN, self.oa.ACCESS_TOKEN_SECRET)
-#         status_code = self.verify_credentials(api)
-#         # 200 means success
-#         self.assertEqual(status_code, 200)
-
-#     def test_oauth_2(self):
-#         """Test application authentication."""
-#         api = TwitterAPI(self.oa.CONSUMER_KEY, self.oa.CONSUMER_SECRET,
-#                          auth_type='oAuth2')
-#         status_code = self.verify_credentials(api)
-#         # 403 means no access, which is correct since no user credentials
-#         # provided
-#         self.assertEqual(status_code, 403)
-
-#     def verify_credentials(self, api):
-#         r = api.request('account/verify_credentials')
-#         return r.status_code
+        txt = []
+        for tweet in res:
+            txt.append(tweet.text)
+        print(txt)
 
 
-# if __name__ == '__main__':
-#     unittest.main()
+        self.assertEqual(1, len(txt))
 
 
-# import mathfun as fun
-from pytest import approx
-
-def test_addints():
-	assert 1+1 == 2
-
-# def test_addfloats():
-# 	assert fun.add(1.5, 1.7) == approx(3.1)
 
 
+if __name__ == '__main__':
+    unittest.main()
 
